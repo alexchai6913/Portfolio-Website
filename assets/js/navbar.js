0e69620bc041f14cc78057100e64ff1b696e7ccf
Alex Chai <148508230+alexchai97@users.noreply.github.com>
2024-10-21 18:54:38 +0800

    fixed navbar logic and added animation

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,13 +1,78 @@
 const menuBtn = document.getElementById('menu');
+const navWrapper = document.getElementById('nav-wrapper');
 let menuActive = false;
 
-menuBtn.onclick = function() {
-    menuActive = !menuActive;
-    
-    if(menuActive){
-        menuBtn.innerText = 'close';
-        return;
-    }
 
-    menuBtn.innerText = 'menu';
+const navbarTL = gsap.timeline({
+    defaults: {
+        ease: "power2.out",
+        duration: .3
+    }
+});
+
+navbarTL.pause();
+
+navbarTL.to([navWrapper],
+    {
+        x: 0,
+        opacity: 1,
+        duration: .6
+    })
+    .to(menuBtn,
+        {
+            color: 'white'
+        }, "<"
+    )
+    .to('.nav-link',
+        {
+            padding: 'var(--spacer-2)',
+            stagger: .1
+        }, "-=.5"
+    );
+
+const toggleMenu = () => {
+    menuActive = !menuActive;
+
+    if (menuActive) {
+        menuBtn.innerText = 'close';
+        navbarTL.play();
+    } else {
+        menuBtn.innerText = 'menu';
+        navbarTL.reverse();
+    }
 };
+
+
+menuBtn.onclick = toggleMenu;
+
+// Revert navbar if resized when menu is active
+window.addEventListener('resize', () => {
+    //check if menu is active and menu button is not visible
+    if (menuActive && window.getComputedStyle(menuBtn).display === 'none') {
+        menuActive = false;
+        gsap.to(navWrapper,
+            {
+                x: 0,
+                opacity: 1,
+                duration: 0.6
+            })
+        gsap.to('.nav-link',
+            {
+                x: 0,
+                padding: 0
+            })
+    }
+});
+
+const smoothScroll = (target) => {
+
+    if (menuActive && window.getComputedStyle(menuBtn).display !== 'none') {
+        toggleMenu();
+        setTimeout(() => {
+            lenis.scrollTo(target);
+        }, 200); // Wait until menu is closed before scroll
+    } else {
+        lenis.scrollTo(target);
+    }
+};
+
