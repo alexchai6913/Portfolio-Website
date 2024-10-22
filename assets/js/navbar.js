e38022c4a860d884a757191b4bf9204ae86d8982
Alex Chai <148508230+alexchai97@users.noreply.github.com>
2024-10-22 09:24:57 +0800

    Fix nav wrapper not reappear on orientation change

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -32,7 +32,10 @@ navbarTL.to([navWrapper],
 
 const toggleMenu = () => {
     menuActive = !menuActive;
+    refreshMenu();
+};
 
+const refreshMenu = () => {
     if (menuActive) {
         menuBtn.innerText = 'close';
         navbarTL.play();
@@ -40,29 +43,31 @@ const toggleMenu = () => {
         menuBtn.innerText = 'menu';
         navbarTL.reverse();
     }
-};
+}
 
 
 menuBtn.onclick = toggleMenu;
 
-// Revert navbar if resized when menu is active
-window.addEventListener('resize', () => {
-    //check if menu is active and menu button is not visible
-    if (menuActive && window.getComputedStyle(menuBtn).display === 'none') {
-        menuActive = false;
-        gsap.to(navWrapper,
-            {
-                x: 0,
-                opacity: 1,
-                duration: 0.6
-            })
-        gsap.to('.nav-link',
-            {
-                x: 0,
-                padding: 0
-            })
+window.addEventListener('resize', handleResize);
+window.addEventListener('orientationchange', handleResize);
+
+function handleResize() {
+    refreshMenu();
+
+    // Check if menu is active and menu button is not visible
+    if (window.getComputedStyle(menuBtn).display === 'none') {
+        gsap.to(navWrapper, {
+            x: 0,
+            opacity: 1,
+            duration: 0.6
+        });
+        gsap.to('.nav-link', {
+            x: 0,
+            padding: 0
+        });
     }
-});
+}
+
 
 const smoothScroll = (target) => {
 
